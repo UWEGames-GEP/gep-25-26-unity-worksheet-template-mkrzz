151ea3a8dd4b3b5750cf06a80ab42d0a968e4c06
Matt <Matthew2.Krzywon@live.uwe.ac.uk>
2025-10-08 14:06:57 +0100

    Completed FSM worksheet task

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
+
+
 public class GameManager : MonoBehaviour
 {
+   
+    public enum GameState { PAUSE, GAMEPLAY };
+    public GameState state;
+    private bool hasChangedState;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,44 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (state == GameState.GAMEPLAY)
+        {
+            hasChangedState = true;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+
+            {
+                
+                state = GameState.PAUSE;
+            }
+
+        }
+        else if (state == GameState.PAUSE)
+
+        { 
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = GameState.GAMEPLAY;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (hasChangedState)
+        {
+            hasChangedState = false;
+
+            if (state == GameState.GAMEPLAY)
+            {
+                Time.timeScale = 1.0f;
+            }
+            else if (state == GameState.PAUSE)
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+            
+            
     }
 }
