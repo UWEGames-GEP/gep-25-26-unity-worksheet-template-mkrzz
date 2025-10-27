4d4b9905c7792ea6e3a9b011846cda47d8291d0c
Matt <Matthew2.Krzywon@live.uwe.ac.uk>
2025-10-27 16:30:00 +0000

    Implemented inventory script - disabled the script when entering pause state

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,6 +51,11 @@ public class GameManager : MonoBehaviour
             Debug.Log("Paused");
             Time.timeScale = 0f;
 
+            foreach (var inv in Object.FindObjectsByType<Inventory>(FindObjectsSortMode.None))
+            {
+                inv.enabled = false;
+            }
+
         }
 
         public void Update(GameManager gm)
@@ -64,7 +69,10 @@ public class GameManager : MonoBehaviour
         public void Exit(GameManager gm)
         {
 
-            
+            foreach (var inv in Object.FindObjectsByType<Inventory>(FindObjectsSortMode.None))
+            {
+                inv.enabled = true;
+            }
 
         }
 
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+
+
+
+public class Inventory : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
+
+    public List<string> items = new List<string>();
+    string itemName;
+   
+
+   
+
+    public void Add(string itemName)
+    {
+
+        items.Add(itemName);
+        
+    }
+
+    public void Remove(string itemName)
+    {
+
+        items.Remove(itemName);
+        
+    }
+    
+   
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+            AddItemToInventory("Generic Item");
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            RemoveItemFromInventory("Generic Item");            
+
+        }
+    }
+
+
+    private void AddItemToInventory(string v)
+    {
+
+        Add(v);
+                
+    }
+
+    private void RemoveItemFromInventory(string v)
+    {
+     
+        Remove(v);
+        
+    }
+
+}
