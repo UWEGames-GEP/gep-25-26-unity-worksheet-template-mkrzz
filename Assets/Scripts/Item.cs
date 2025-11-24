196c67a5be411730ec11bf16d4752ee30018ce34
Matt <Matthew2.Krzywon@live.uwe.ac.uk>
2025-11-24 14:09:11 +0000

    Reimported gem pack

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,8 +4,9 @@ using UnityEngine.Audio;
 public class Item : MonoBehaviour
 {
 
-    private SphereCollider itemCollider;
+    private Collider itemCollider;
     private MeshRenderer mr;
+
     public string itemName;
     public string itemDescription;
     public AudioSource audioSource;
@@ -13,14 +14,17 @@ public class Item : MonoBehaviour
 
     void Start()
     {
+
+        //looks for these components on the game object
         audioSource = GetComponent<AudioSource>();
-        itemCollider = GetComponent<SphereCollider>();
+        itemCollider = GetComponent<Collider>();
         mr = GetComponent<MeshRenderer>();
 
     }
 
     private void OnTriggerEnter (Collider other)
     {
+
         if (other.gameObject.tag == "Player")
         {
 
@@ -32,11 +36,14 @@ public class Item : MonoBehaviour
             }
 
             audioSource.Play();
+
+            //disables the collider and mesh renderer 
             itemCollider.enabled = false;
             mr.enabled = false;
             
 
         }
+
     }
 
 
