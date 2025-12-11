619c5239a6770cdaa8d246888c60c29258c95f22
Matt <Matthew2.Krzywon@live.uwe.ac.uk>
2025-12-11 12:50:52 +0000

    Added UI interaction text with crate - Press 'E' to open when player enters box collision

--- Assets/Scripts/LootCrateInteraction.cs
+++ Assets/Scripts/LootCrateInteraction.cs
@@ -1,12 +1,31 @@
 using UnityEngine;
+using TMPro;
 
 public class LootCrateInteraction : MonoBehaviour
 {
     public LootCrateUI lootCrateUI;
     private bool playerInRange;
+    public TextMeshProUGUI interactionHint;
+    private Camera mainCamera;
+    public Vector3 hintOffset = new Vector3(0, 1f, 0);
+
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
 
     void Update()
     {
+
+        // Follow the crate
+        if (interactionHint.gameObject.activeSelf)
+        {
+            Vector3 screenPos = mainCamera.WorldToScreenPoint(transform.position + hintOffset);
+            interactionHint.transform.position = screenPos;
+        }
+
         if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             if (!lootCrateUI.IsVisible())
@@ -22,16 +41,25 @@ public class LootCrateInteraction : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
             playerInRange = true;
+
+        if (interactionHint != null)
+            interactionHint.gameObject.SetActive(true);
+
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+
             playerInRange = false;
             lootCrateUI.Hide();
+            if (interactionHint != null)
+                interactionHint.gameObject.SetActive(false);
+
         }
     }
 
